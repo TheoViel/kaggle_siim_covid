Add effnetv2 + fix scaling issue

src/model_zoo/encoders.py
@@ -1,7 +1,7 @@
+import timm
 import torch
 import torch.nn.functional as F
 import resnest.torch as resnest_torch
-from efficientnet_pytorch import EfficientNet
 
 
 def get_encoder(name):
@@ -22,18 +22,26 @@ def get_encoder(name):
         model = torch.hub.load("facebookresearch/WSL-Images", name)
     elif "resnext" in name or "resnet" in name or "densenet" in name:
         model = torch.hub.load("pytorch/vision:v0.6.0", name, pretrained=True)
-    elif "efficientnet" in name:
-        model = EfficientNet.from_pretrained(name)
+    elif "efficientnetv2" in name:
+        model = getattr(timm.models, name)(
+            pretrained=True,
+            drop_path_rate=0.2,
+        )
     else:
         raise NotImplementedError
 
-    if "efficientnet" in name:
-        model.nb_ft = model._fc.in_features
+    if "efficientnetv2" in name:
+        # model.nb_ft = model.classifier.in_features
+        model.nb_ft = model.blocks[6][-1].conv_pwl.out_channels
+        model.nb_ft_int = model.blocks[4][-1].conv_pwl.out_channels
+        model.extract_features = lambda x: extract_features_efficientnet(model, x)
     elif "densenet" in name:
         model.nb_ft = model.classifier.in_features
+        model.nb_ft_int = model.nb_ft // 2
         model.extract_features = lambda x: extract_features_densenet(model, x)
     else:
         model.nb_ft = model.fc.in_features
+        model.nb_ft_int = model.nb_ft // 2
         model.extract_features = lambda x: extract_features_resnet(model, x)
 
     model.name = name
@@ -58,3 +66,18 @@ def extract_features_densenet(self, x):
     x = self.features(x)
     x = F.relu(x, inplace=True)   # remove ?
     return x
+
+
+def extract_features_efficientnet(self, x):
+    x = self.conv_stem(x)
+    x = self.bn1(x)
+    x = self.act1(x)
+
+    features = []
+    for i, b in enumerate(self.blocks):
+        x = b(x)
+        if i in [1, 2, 4, 6]:
+            # print(x.size())
+            features.append(x)
+
+    return features

src/model_zoo/models.py
@@ -35,7 +35,7 @@ class CovidModel(nn.Module):
         super().__init__()
         self.encoder = encoder
         self.num_classes = num_classes
-        self.nb_ft = encoder.fc.in_features
+        self.nb_ft = encoder.nb_ft
 
         if reduce_stride:
             if "resnext" in self.encoder.name:
@@ -47,7 +47,7 @@ class CovidModel(nn.Module):
             else:
                 raise NotImplementedError
 
-        self.mask_head_3 = self.get_mask_head(self.nb_ft // 2)
+        self.mask_head_3 = self.get_mask_head(self.encoder.nb_ft_int)
         self.mask_head_4 = self.get_mask_head(self.nb_ft)
 
         self.key_conv = nn.Conv2d(1, 1, kernel_size=3, padding=1)
@@ -102,7 +102,8 @@ class CovidModel(nn.Module):
         mask_4 = self.mask_head_4(x4)
         masks = [mask_3, mask_4]
 
-        pooled = self.attention_mechanism(x4, masks)
+        # pooled = self.attention_mechanism(x4, masks)
+        pooled = x4.mean(-1).mean(-1)
 
         logits_img = self.logits_img(pooled)
         logits_study = self.logits_study(pooled)

src/training/main.py
@@ -8,7 +8,7 @@ from training.train import fit
 from data.dataset import CovidClsDataset
 from data.transforms import get_transfos_cls
 from model_zoo.models import get_model
-from utils.metrics import per_class_average_precision_score
+from utils.metrics import per_class_average_precision_score, study_level_map
 from utils.torch import seed_everything, count_parameters, save_model_weights
 
 
@@ -143,13 +143,14 @@ def k_fold(config, df, df_extra=None, log_folder=None):
         np.save(log_folder + "pred_oof_study.npy", pred_oof_study)
         np.save(log_folder + "pred_oof_img.npy", pred_oof_img)
 
-    score_study = per_class_average_precision_score(
-        pred_oof_study, df[CLASSES].values, num_classes=config.num_classes
+    score_study = study_level_map(
+        pred_oof_study, df[CLASSES].values, df["study_id"]
     )
+
     score_img = per_class_average_precision_score(pred_oof_img, df["img_target"].values)
 
-    print(' -> CV Scores :')
-    print(f'Study : {score_study :.4f}')
-    print(f'Image : {score_img :.4f}')
+    print('CV Scores :')
+    print(f' -> Study : {score_study :.4f}')
+    print(f' -> Image : {score_img :.4f}')
 
     return pred_oof_study, pred_oof_img

src/training/optim.py
@@ -120,18 +120,21 @@ class CovidLoss(nn.Module):
         self.focal_tversky = FocalTverskyLoss()
 
         self.w_bce = 0.75
-        self.seg_loss_w = 0.5
+        self.w_seg_loss = 0.5
+        self.w_study = 2
+        self.w_img = 1
 
     def compute_seg_loss(self, preds, truth):
-        truth = F.interpolate(
-            truth.unsqueeze(1), size=preds[0].size()[-2:], mode='bilinear', align_corners=False
-        )
+        losses = []
+        truth = truth.unsqueeze(1)
 
-        losses = [
-            self.w_bce * self.bce(pred, truth).mean((1, 2, 3)) +
-            (1 - self.w_bce) * self.focal_tversky(pred, truth)
-            for pred in preds
-        ]
+        for pred in preds:
+            truth = F.interpolate(
+                truth, size=pred.size()[-2:], mode='bilinear', align_corners=False
+            )
+            loss = self.w_bce * self.bce(pred, truth).mean((1, 2, 3))
+            loss += (1 - self.w_bce) * self.focal_tversky(pred, truth)
+            losses.append(loss)
 
         return torch.stack(losses, -1).sum(-1)
 
@@ -141,7 +144,7 @@ class CovidLoss(nn.Module):
 
         seg_loss = self.compute_seg_loss(preds_mask, y_mask)
 
-        study_loss = self.ce(pred_study, y_study.long())
-        img_loss = self.bce(pred_img.view(y_img.size()), y_img)
+        study_loss = self.w_study * self.ce(pred_study, y_study.long())
+        img_loss = self.w_img * self.bce(pred_img.view(y_img.size()), y_img)
 
-        return self.seg_loss_w * seg_loss + (1 - self.seg_loss_w) * (img_loss + study_loss)
+        return self.w_seg_loss * seg_loss + (1 - self.w_seg_loss) * (img_loss + study_loss)

src/training/train.py
@@ -3,7 +3,7 @@ import torch
 import numpy as np
 from transformers import get_linear_schedule_with_warmup
 
-from utils.metrics import per_class_average_precision_score
+from utils.metrics import per_class_average_precision_score, study_level_map
 from training.loader import define_loaders
 # from training.mix import cutmix_data, mixup_data
 from training.optim import CovidLoss, define_optimizer
@@ -123,9 +123,13 @@ def fit(
                 avg_loss += loss.item() / len(train_loader)
 
                 scaler.step(optimizer)
+
+                scale = scaler.get_scale()
                 scaler.update()
 
-                scheduler.step()
+                if scale == scaler.get_scale():
+                    scheduler.step()
+
                 for param in model.parameters():
                     param.grad = None
 
@@ -163,8 +167,8 @@ def fit(
                     preds_study = np.concatenate([preds_study, pred_study.cpu().numpy()])
                     preds_img = np.concatenate([preds_img, pred_img.cpu().numpy()])
 
-                study_ap = per_class_average_precision_score(
-                    preds_study, val_dataset.study_targets, num_classes=num_classes
+                study_ap = study_level_map(
+                    preds_study, val_dataset.study_targets, val_dataset.studies
                 )
                 img_ap = per_class_average_precision_score(preds_img, val_dataset.img_targets)
 
@@ -180,7 +184,7 @@ def fit(
 
             if (epoch + 1 >= first_epoch_eval) or (epoch + 1 == epochs):
                 print(
-                    f"val_loss={avg_val_loss:.3f} \t study_ap={study_ap:.3f} \t img_ap={img_ap:.3f}"
+                    f"val_loss={avg_val_loss:.3f}\t study_map={study_ap:.3f}\t img_map={img_ap:.3f}"
                 )
             else:
                 print("")